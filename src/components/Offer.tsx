3bc5406f462fe5547bb24e1f01cd34916f2b0df6
Vincent <75339867+Vincent-Lin-UF@users.noreply.github.com>
2023-10-18 13:00:58 -0400

    Menu

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -12,7 +12,6 @@ const Offer = () => {
           Comes with Pork Fried Rice and Egg Roll
         </p>
         <CountDown/>
-        <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="flex-1 w-full relative md:h-full">
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link"; 
 import React, { useEffect, useState } from "react";
 
 const data = [
@@ -19,27 +20,28 @@ const data = [
     image: "/food3.jpg",
   },
 ];
-  
 
 const Slider = () => {
-    const[currentSlide, setCurrentSlide] = useState(0);
+  const[currentSlide, setCurrentSlide] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(
-            () => setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
-            4000
-        );
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+      const interval = setInterval(
+          () => setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+          4000
+      );
+      return () => clearInterval(interval);
+  }, []);
 
-   return (
+  return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       {/* TEXT CONTAINER */}
       <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
         <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
           {data[currentSlide].title}
         </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Menu</button>
+        <Link href="/menu"> 
+          <button className="bg-red-500 text-white py-4 px-8">Menu</button>
+        </Link>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="w-full flex-1 relative">
